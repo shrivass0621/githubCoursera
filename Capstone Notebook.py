4578ff6b1da8eff729724261d04d7500484da6de
shrivass0621 <67771952+shrivass0621@users.noreply.github.com>
2020-08-06 22:01:04 -0700

    Add files via upload

---
+++ Capstone Notebook.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This is the notebook I will be using to finish Course 9 Capstone Project of the IBM Data Science Project
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
